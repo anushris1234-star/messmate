bb21fd14ff63059a239426e129912c050a7c7a08
Ananya Shrivastava <anushris1234@gmail.com>
2026-09-06 00:51:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>MessMate 🍱</h1>
+      <p>Mess Feedback & Management System</p>
+
+      <button>View Menu</button>
+      <button>Give Feedback</button>
+      <button>Report an Issue</button>
+    </div>
+  )
+}
+
+export default App
